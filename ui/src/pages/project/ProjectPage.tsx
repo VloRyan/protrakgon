fd0a540cb0d9e24996bbafcc4a155ca49e33de4c
VloRyan <florian685@gmail.com>
2025-09-02 16:12:03 +0200

    feat: disable activity and slot card on new project

--- ui/src/pages/project/ProjectPage.tsx
+++ ui/src/pages/project/ProjectPage.tsx
@@ -82,6 +82,7 @@ export function ProjectPage() {
   } else if (fetchOpts.page.limit === undefined) {
     fetchOpts.page.limit = -1;
   }
+  const projectId = form.getValue("id") as string | null;
   return (
     <ItemPage error={error} isLoading={isLoading} formId={form.id}>
       <form {...form.setup()}>
@@ -89,7 +90,8 @@ export function ProjectPage() {
       </form>
       <ActivitiesCard
         className="mt-2 mx-2"
-        fetchOpts={{ filter: { projectId: form.getValue("id") as string } }}
+        fetchOpts={{ filter: { projectId: projectId } }}
+        disabled={projectId == null}
       />
       <SlotCard
         className="mt-2 mx-2"
@@ -102,6 +104,7 @@ export function ProjectPage() {
           includes: fetchOpts.includes,
           sort: "-start",
         }}
+        disabled={projectId == null}
       />
     </ItemPage>
   );
@@ -109,38 +112,57 @@ export function ProjectPage() {
 
 interface ActivitiesCardProps extends CardProps {
   fetchOpts: FetchOpts;
+  disabled?: boolean;
 }
-const ActivitiesCard = ({ fetchOpts, ...cardProps }: ActivitiesCardProps) => {
+const ActivitiesCard = ({
+  fetchOpts,
+  disabled,
+  ...cardProps
+}: ActivitiesCardProps) => {
   const [location] = useLocation();
   return (
     <Card {...cardProps}>
       <Card.Title className="text-center">
         <Row className="pt-2">
-          <Col className="d-flex justify-content-start ps-4 fw-bold">
+          <Col
+            className={
+              "d-flex justify-content-start ps-4" +
+              (!disabled ? " fw-bold" : " text-secondary")
+            }
+          >
             Activities
           </Col>
           <Col className="d-flex justify-content-center" xs="auto">
-            <CreateButton size="sm" href={location + "/activity/new"} />
+            <CreateButton
+              size="sm"
+              title={!disabled ? "Create activity" : undefined}
+              href={location + "/activity/new"}
+              disabled={disabled}
+            />
           </Col>
           <Col className="d-flex justify-content-end me-2 fs-6 small"></Col>
         </Row>
       </Card.Title>
       <Card.Body>
-        <ActivityList
-          resourcesUrl={apiPath(location, "activity")}
-          locationUrl={joinPath(location, "activity")}
-          fetchOpts={fetchOpts}
-        />
+        {!disabled ? (
+          <ActivityList
+            resourcesUrl={apiPath(location, "activity")}
+            locationUrl={joinPath(location, "activity")}
+            fetchOpts={fetchOpts}
+          />
+        ) : null}
       </Card.Body>
     </Card>
   );
 };
 
 interface SlotCardProps extends CardProps {
+  disabled?: boolean;
   csvFileNamePrefix: string;
   fetchOpts: FetchOpts;
 }
 const SlotCard = ({
+  disabled,
   csvFileNamePrefix,
   fetchOpts,
   ...cardProps
@@ -163,20 +185,28 @@ const SlotCard = ({
     <Card {...cardProps}>
       <Card.Title className="text-center">
         <Row className="pt-2">
-          <Col className="d-flex justify-content-start ps-4 fw-bold">Slots</Col>
+          <Col
+            className={
+              "d-flex justify-content-start ps-4" +
+              (!disabled ? " fw-bold" : " text-secondary")
+            }
+          >
+            Slots
+          </Col>
           <Col className="d-flex justify-content-center" xs="auto">
             <IconButton
               icon={faPlus}
               variant="outline-primary"
               size="sm"
               href={location + "/slot/new"}
+              disabled={disabled}
             />
             <IconButton
               icon={faFileExport}
               variant="outline-primary"
               size="sm"
               className="ms-2"
-              title="Download .csv file"
+              title={!disabled ? "Download .csv file" : undefined}
               download={filenameFromFilter(fetchOpts.filter, csvFileNamePrefix)}
               href={apiPath(
                 location,
@@ -185,29 +215,36 @@ const SlotCard = ({
                     filter: fetchOpts.filter,
                   } satisfies FetchOpts),
               )}
+              disabled={disabled}
             />
-
             <IconButton
               icon={faMinimize}
               variant={minimizeText ? "outline-primary" : "primary"}
               size="sm"
-              title="Minimize text in columns (disable wrapping)"
+              title={
+                !disabled
+                  ? "Minimize text in columns (disable wrapping)"
+                  : undefined
+              }
               className="ms-2"
               onClick={() => setMinimizeText(!minimizeText)}
+              disabled={disabled}
             />
           </Col>
 
           <Col className="d-flex justify-content-end me-2 fs-6 small align-items-center">
-            <b>Filter:</b>
-            {fetchOpts.filter ? (
-              <div className="ps-1">
-                {formatTimespan(
-                  fetchOpts.filter.from as string,
-                  fetchOpts.filter.fromComparator as number,
-                  fetchOpts.filter.until as string,
-                  fetchOpts.filter.untilComparator as number,
-                )}
-              </div>
+            {fetchOpts.filter && !disabled ? (
+              <>
+                <b>Filter:</b>
+                <div className="ps-1">
+                  {formatTimespan(
+                    fetchOpts.filter.from as string,
+                    fetchOpts.filter.fromComparator as number,
+                    fetchOpts.filter.until as string,
+                    fetchOpts.filter.untilComparator as number,
+                  )}
+                </div>
+              </>
             ) : null}
             {fetchOpts.filter?.billable === true ? (
               <div className="ps-1">
@@ -226,26 +263,31 @@ const SlotCard = ({
               size="sm"
               id="button-search"
               onClick={() => setShowSearchBar(true)}
+              disabled={disabled}
             />
           </Col>
         </Row>
       </Card.Title>
       <Card.Body>
-        <SlotList
-          resourcesUrl={apiPath(location, "slot")}
-          locationUrl={location}
-          fetchOpts={fetchOpts}
-          wrapText={minimizeText}
-        ></SlotList>
-        <SearchBar
-          show={showSearchBar}
-          setShow={setShowSearchBar}
-          content={SlotSearchBarContent}
-          filter={
-            { slots: fetchOpts.filter as ObjectLike } satisfies ObjectLike
-          }
-          onBeforeSearch={ValidateSlotsForm}
-        />
+        {!disabled ? (
+          <>
+            <SlotList
+              resourcesUrl={apiPath(location, "slot")}
+              locationUrl={location}
+              fetchOpts={fetchOpts}
+              wrapText={minimizeText}
+            ></SlotList>
+            <SearchBar
+              show={showSearchBar}
+              setShow={setShowSearchBar}
+              content={SlotSearchBarContent}
+              filter={
+                { slots: fetchOpts.filter as ObjectLike } satisfies ObjectLike
+              }
+              onBeforeSearch={ValidateSlotsForm}
+            />
+          </>
+        ) : null}
       </Card.Body>
     </Card>
   );
