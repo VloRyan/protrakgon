fc21c9e8c079da7a3f564ecc7d74983bd69d0be5
VloRyan <florian685@gmail.com>
2025-10-09 09:50:12 +0200

    test: setup locale

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    setupFiles: ["./setupVitest.ts"],
-  },
   plugins: [react()],
   server: {
     proxy: {
