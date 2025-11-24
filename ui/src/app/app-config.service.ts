4544f95cb707524e5da5a2b879e9ddb68e9af938
VloRyan <florian685@gmail.com>
2025-11-24 16:09:00 +0100

    fix: local api url

--- ui/src/app/app-config.service.ts
+++ ui/src/app/app-config.service.ts
@@ -6,7 +6,7 @@ import {Injectable} from '@angular/core';
 export class AppConfigService {
   apiUrl(){
     // @ts-expect-error will be added by server on prod
-  return  window.SERVER_DATA != undefined ?  window.SERVER_DATA.apiUrl:"/api/";
+  return  window.SERVER_DATA != undefined ?  window.SERVER_DATA.apiUrl:"/api/v1/";
   }
 
   contextRoot(){
