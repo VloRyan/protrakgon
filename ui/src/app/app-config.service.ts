273d20fd15c20989054ec57f46359385bcd8301d
VloRyan <florian685@gmail.com>
2025-12-09 13:57:19 +0100

    build: update ui dependencies
    fixs ci bugs
    disabled karma tests...

--- ui/src/app/app-config.service.ts
+++ ui/src/app/app-config.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
-
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+declare const window: any; // @see https://stackoverflow.com/questions/12709074/how-do-you-explicitly-set-a-new-property-on-window-in-typescript/56402425#56402425
 @Injectable({
   providedIn: 'root',
 })
 export class AppConfigService {
   apiUrl() {
-    // @ts-expect-error will be added by server on prod
     return window.SERVER_DATA != undefined
       ? window.SERVER_DATA.apiUrl
       : '/api/v1/';
   }
 
   contextRoot() {
-    // @ts-expect-error will be added by server on prod
     return window.SERVER_DATA != undefined
       ? window.SERVER_DATA.contextRoot
       : '/';
